41e4d26b3a15510f3adde52661fccf9f43d57a9c
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2024-08-21 15:48:30 +0200

    Update termux-env-bootstrap.sh

--- termux-env-bootstrap.sh
+++ termux-env-bootstrap.sh
@@ -4,18 +4,19 @@
 pkg update
 pkg upgrade -y
 pkg install -y \
-    gh
+    gh \
+    git
 
 # login to gh
 gh auth login -h GitHub.com -p ssh
-
-# clone this repo
-gh repo clone ferranpujolcamins/termux-bootstrap ~/.termux-bootstrap
 
 # setup git
 eval $(gh gist view --raw 7be636e10838e7454338af935bf66beb | tail -n +2)
 git config --global gpg.format ssh
 git config --global user.signingkey ~/.ssh/id_ed25519.pub
+
+# clone this repo
+gh repo clone ferranpujolcamins/termux-bootstrap ~/.termux-bootstrap
 
 # add scripts to path
 touch ~/.bashrc
