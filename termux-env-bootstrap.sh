cd31d101dca11506c73fd907c04d7e982986695d
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2024-08-22 13:08:35 +0200

    Fix termux-env-bootstrap.sh

--- termux-env-bootstrap.sh
+++ termux-env-bootstrap.sh
@@ -12,28 +12,28 @@ pkg install -y \
     gh \
     git
 
-echo "Login to GH:"
+echo "Login to GH to set ssh key:"
 gh auth login -h GitHub.com -p ssh
-
-echo "Clone the repo:"
-gh repo clone ferranpujolcamins/termux-bootstrap ~/.termux-bootstrap
-
-echo "Set up git:"
-rm -rf ~/.termux-bootstrap/
-gh gist clone 7be636e10838e7454338af935bf66beb ~/.termux-bootstrap/.git_config
-chmod +x ~/.termux-bootstrap/.git_config/git_config.sh
-~/.termux-bootstrap/.git_config/git_config.sh
-
-echo "Uninstall gh and clean tokens"
 pkg uninstall gh
 rm -rf ~/.config/gh
 
+echo "Clone the repo:"
+rm -rf ~/.termux-bootstrap/
+gh repo clone ferranpujolcamins/termux-bootstrap ~/.termux-bootstrap
+
+echo "Set up git:"
+git clone git@gist.github.com:7be636e10838e7454338af935bf66beb.git \
+    ~/.termux-bootstrap/.git_config
+chmod +x ~/.termux-bootstrap/.git_config/git_config.sh
+~/.termux-bootstrap/.git_config/git_config.sh
+
 echo "Add scripts to path:"
 rm -f ~/.zshrc
-echo "export PATH=$PATH:~/.termux-bootstrap" >> ~/.bashrc
+echo "export PATH=$PATH:~/.termux-bootstrap" >> ~/.zshrc
 source ~/.zshrc
 
 echo "Setup ZSH"
+pkg install zsh
 chsh -s zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
