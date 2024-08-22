6fb90e266f990706f4bb83700aea08ec9caef2a0
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2024-08-22 12:54:26 +0200

    Improve scripts and add zsh shell

--- termux-env-bootstrap.sh
+++ termux-env-bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Set inconsolata font"
+rm -f .termux/font.ttf
 curl -o .termux/font.ttf https://raw.githubusercontent.com/googlefonts/Inconsolata/fc1fc21081558b39a2db43bfd9b65bf9acb50701/fonts/ttf/Inconsolata-Regular.ttf
 termux-reload-settings
 
@@ -18,6 +19,7 @@ echo "Clone the repo:"
 gh repo clone ferranpujolcamins/termux-bootstrap ~/.termux-bootstrap
 
 echo "Set up git:"
+rm -rf ~/.termux-bootstrap/
 gh gist clone 7be636e10838e7454338af935bf66beb ~/.termux-bootstrap/.git_config
 chmod +x ~/.termux-bootstrap/.git_config/git_config.sh
 ~/.termux-bootstrap/.git_config/git_config.sh
@@ -27,9 +29,13 @@ pkg uninstall gh
 rm -rf ~/.config/gh
 
 echo "Add scripts to path:"
-touch ~/.bashrc
+rm -f ~/.zshrc
 echo "export PATH=$PATH:~/.termux-bootstrap" >> ~/.bashrc
-source ~/.bashrc
+source ~/.zshrc
+
+echo "Setup ZSH"
+chsh -s zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Call setup script:"
 termux-env-setup
