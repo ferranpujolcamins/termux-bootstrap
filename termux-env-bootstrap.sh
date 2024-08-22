1e6951bd8e63988861f641634c8ef9d0e4b70811
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2024-08-22 16:38:46 +0200

    Update termux-env-bootstrap.sh

--- termux-env-bootstrap.sh
+++ termux-env-bootstrap.sh
@@ -1,39 +1,60 @@
 #!/bin/sh
 set -e
 
-echo "Set inconsolata font"
+echo ""
+echo "===================="
+echo "Set inconsolata font:"
+echo "===================="
 rm -f .termux/font.ttf
 curl -fsSL -o .termux/font.ttf https://raw.githubusercontent.com/googlefonts/Inconsolata/fc1fc21081558b39a2db43bfd9b65bf9acb50701/fonts/ttf/Inconsolata-Regular.ttf
 termux-reload-settings
 
+echo ""
+echo "================="
 echo "Install GH & git:"
+echo "================="
 pkg update
 pkg upgrade -y
 pkg install -y \
     gh \
     git
 
+echo ""
+echo "==========================="
 echo "Login to GH to set ssh key:"
+echo "==========================="
 gh auth login -h GitHub.com -p ssh
 pkg uninstall -y gh
 rm -rf ~/.config/gh
 
+echo ""
+echo "==============="
 echo "Clone the repo:"
+echo "==============="
 rm -rf ~/.termux-bootstrap/
 git clone git@github.com:ferranpujolcamins/termux-bootstrap.git ~/.termux-bootstrap
 
+echo ""
+echo "==========="
 echo "Set up git:"
+echo "==========="
 git clone git@gist.github.com:7be636e10838e7454338af935bf66beb.git \
     ~/.termux-bootstrap/.git_config
 chmod +x ~/.termux-bootstrap/.git_config/git_config.sh
 ~/.termux-bootstrap/.git_config/git_config.sh
 
-echo "Setup ZSH"
+echo ""
+echo "=========="
+echo "Setup ZSH:"
+echo "=========="
 cp ~/.termux-bootstrap/.zshrc ~/.zshrc
 pkg install -y zsh
 # The oh-my-zsh installer already activates zsh shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
     "" --keep-zshrc --unattended
 
+echo ""
+echo "=================="
 echo "Call setup script:"
+echo "=================="
 termux-env-setup
